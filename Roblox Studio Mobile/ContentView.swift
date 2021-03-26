06e0900e716555bbbe8d0ea60f71de6b54ff9ccd
Andy <alin21@stuy.edu>
2021-03-25 20:13:24 -0400

    Initial Commit

---
+++ Roblox Studio Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Roblox Studio Mobile
+//
+//  Created by Andy Lin on 3/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Roblox Studio Mobile/Roblox_Studio_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roblox_Studio_MobileApp.swift
+//  Roblox Studio Mobile
+//
+//  Created by Andy Lin on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Roblox_Studio_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
